03da2b0efccd8e7c8a6de50f610975404fee7ce2
Pratham Singh <pratham.gate2015@gmail.com>
2022-07-10 12:26:13 +0530

    Create PrintPattern5.java
    Printing Pattern:
    
    *
    **
    ***
    ****
    ***
    **
    *

---
+++ PrintPattern5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+
+Printing Pattern:
+
+*
+**
+***
+****
+***
+**
+*
+
+*/
+
+public class PrintPattern {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        PrintGivenPattern(n);
+        input.close();
+    }
+
+    static void PrintGivenPattern(int n){
+        // Print outer loop for rows
+        for (int i = 1; i <= 2*n-1; i++) {
+            int totalCol = i<=n?i:2*n-i;
+            for (int j = 1; j <=totalCol; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
